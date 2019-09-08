4ce340d1770c7f9058147251832b7784c5bf243b
Your Name <you@example.com>
2019-09-08 23:13:55 +0800

    sdjhf

---
+++ HelloApplet.java
@@ -0,0 +1,7 @@
+import javax.swing. * ;
+import java.awt. * ;
+public class HelloApplet extends JApplet{
+       public void paint(Graphics g){
+              g.drawString("Hello World",10,10);
+}
+}
